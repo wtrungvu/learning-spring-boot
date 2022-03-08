58a4f9cbf35c7a54d05a9e1457dab3221eaba365
Duc Trung Vu <wtrungvu@gmail.com>
2022-03-08 18:08:19 +0700

    Creating restful api Hello World with Spring Boot ✨

--- learning-spring-boot/src/main/java/com/wtrungvu/learningspringboot/LearningSpringBootApplication.java
+++ learning-spring-boot/src/main/java/com/wtrungvu/learningspringboot/LearningSpringBootApplication.java
@@ -2,12 +2,44 @@ package com.wtrungvu.learningspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class LearningSpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearningSpringBootApplication.class, args);
+	}
+
+	@RestController
+	class Resource {
+
+		@RequestMapping(value = "/", method = RequestMethod.GET)
+		Message getMessage() {
+			return new Message("Hello World");
+		}
+
+	}
+
+	class Message {
+		private final String message;
+
+		public Message(String message) {
+			this.message = message;
+		}
+
+		public String getMessage() {
+			return message;
+		}
+
+		@Override
+		public String toString() {
+			return "Message{" +
+					"message='" + message + '\'' +
+					'}';
+		}
 	}
 
 }
