b1597589db55ba2cedd1417effbe65008c70faa7
Duc Trung Vu <wtrungvu@gmail.com>
2022-03-08 22:59:06 +0700

    Writing Test for Select User By User Uid ✨

--- learning-spring-boot/src/test/java/com/wtrungvu/learningspringboot/dao/FakeDataDaoTest.java
+++ learning-spring-boot/src/test/java/com/wtrungvu/learningspringboot/dao/FakeDataDaoTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -33,7 +35,33 @@ class FakeDataDaoTest {
     }
 
     @Test
-    void selectUserByUserUid() {
+    void shouldSelectUserByUserUid() {
+        UUID annaUserUid = UUID.randomUUID();
+        User annaUser = new User(
+                annaUserUid,
+                "Anna",
+                "Montana",
+                User.Gender.MALE,
+                25,
+                "Anna.Montana@gmail.com"
+        );
+        fakeDataDao.insertUser(annaUserUid, annaUser);
+        assertThat(fakeDataDao.selectAllUsers()).hasSize(2);
+
+        Optional<User> annaUserOptional = fakeDataDao.selectUserByUserUid(annaUserUid);
+        assertThat(annaUserOptional.isPresent()).isTrue();
+
+        /* isEqualToComparingFieldByField() is deprecated */
+//        assertThat(annaUserOptional.get()).isEqualToComparingFieldByField(annaUser);
+
+        /* Use the recursive comparison by calling usingRecursiveComparison() */
+        assertThat(annaUserOptional.get()).usingRecursiveComparison().isEqualTo(annaUser);
+    }
+
+    @Test
+    void shouldNotSelectUserByRandomUserUid() {
+        Optional<User> user = fakeDataDao.selectUserByUserUid(UUID.randomUUID());
+        assertThat(user.isPresent()).isFalse();
     }
 
     @Test
