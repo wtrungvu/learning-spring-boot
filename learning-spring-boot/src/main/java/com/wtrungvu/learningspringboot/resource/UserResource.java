98087e699a1e43796ac74e4bd30049dfa084d2c7
Duc Trung Vu <wtrungvu@gmail.com>
2022-03-09 12:38:25 +0700

    Configuring REST Endpoint Paths✨

--- learning-spring-boot/src/main/java/com/wtrungvu/learningspringboot/resource/UserResource.java
+++ learning-spring-boot/src/main/java/com/wtrungvu/learningspringboot/resource/UserResource.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@RequestMapping(path = "/api/v1/users")
 public class UserResource {
 
     private UserService userService;
