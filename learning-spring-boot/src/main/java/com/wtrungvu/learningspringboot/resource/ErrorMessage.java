a144bac1dba94d249543b145e0cadaf00a7808cb
Duc Trung Vu <wtrungvu@gmail.com>
2022-03-09 21:11:58 +0700

    Migrate the User Resource from Spring MVC completely to use JAX-RS ✨

---
+++ learning-spring-boot/src/main/java/com/wtrungvu/learningspringboot/resource/ErrorMessage.java
@@ -0,0 +1,13 @@
+package com.wtrungvu.learningspringboot.resource;
+
+class ErrorMessage {
+    private String errorMessage;
+
+    public ErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+}
--- learning-spring-boot/src/main/java/com/wtrungvu/learningspringboot/resource/UserResourceResteasy.java
+++ learning-spring-boot/src/main/java/com/wtrungvu/learningspringboot/resource/UserResourceResteasy.java
@@ -3,14 +3,15 @@ package com.wtrungvu.learningspringboot.resource;
 import com.wtrungvu.learningspringboot.model.User;
 import com.wtrungvu.learningspringboot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
@@ -29,6 +30,50 @@ public class UserResourceResteasy {
     @Produces(APPLICATION_JSON)
     public List<User> fetchUsers(@QueryParam("gender") String gender) {
         return userService.getAllUsers(Optional.ofNullable(gender));
+    }
+
+    @GET
+    @Produces(APPLICATION_JSON)
+    @Path("{userUid}")
+    public Response fetchUser(@PathParam("userUid") UUID userUid) {
+        Optional<User> userOptional = userService.getUser(userUid);
+        if (userOptional.isPresent()) {
+            return Response.ok(userOptional.get()).build();
+        }
+        return Response.status(Response.Status.NOT_FOUND)
+                .entity(new ErrorMessage("User " + userUid + " was not found"))
+                .build();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON_VALUE)
+    @Produces(APPLICATION_JSON)
+    public Response insertNewUser(@RequestBody User user) {
+        int result = userService.insertUser(user);
+        return getIntegerResponse(result);
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON_VALUE)
+    @Produces(MediaType.APPLICATION_JSON_VALUE)
+    public Response updateUser(@RequestBody User user) {
+        int result = userService.updateUser(user);
+        return getIntegerResponse(result);
+    }
+
+    @DELETE
+    @Produces(MediaType.APPLICATION_JSON_VALUE)
+    @Path("{userUid}")
+    public Response deleteUser(@PathParam("userUid") UUID userUid) {
+        int result = userService.removeUser(userUid);
+        return getIntegerResponse(result);
+    }
+
+    private Response getIntegerResponse(int result) {
+        if (result == 1) {
+            return Response.ok().build();
+        }
+        return Response.status(Response.Status.BAD_REQUEST).build();
     }
 
 }
