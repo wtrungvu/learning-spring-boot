642d1d87aaae4298cc41b8e71f2613d204e580b4
Duc Trung Vu <wtrungvu@gmail.com>
2022-03-09 12:31:23 +0700

    Creating First Restful Api Layer ✨

---
+++ learning-spring-boot/src/main/java/com/wtrungvu/learningspringboot/resource/UserResource.java
@@ -0,0 +1,27 @@
+package com.wtrungvu.learningspringboot.resource;
+
+import com.wtrungvu.learningspringboot.model.User;
+import com.wtrungvu.learningspringboot.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserResource {
+
+    private UserService userService;
+
+    @Autowired
+    public UserResource(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<User> fetchUsers() {
+        return userService.getAllUsers();
+    }
+
+}
