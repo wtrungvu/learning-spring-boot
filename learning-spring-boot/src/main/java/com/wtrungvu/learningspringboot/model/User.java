d12110bca8a71925a1aa5f2d256380cbe25491c2
Duc Trung Vu <wtrungvu@gmail.com>
2022-03-10 00:50:46 +0700

    Writing First Spring Boot Integration Test ✨

--- learning-spring-boot/src/main/java/com/wtrungvu/learningspringboot/model/User.java
+++ learning-spring-boot/src/main/java/com/wtrungvu/learningspringboot/model/User.java
@@ -1,10 +1,12 @@
 package com.wtrungvu.learningspringboot.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.time.LocalDate;
 import java.util.UUID;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class User {
 
     // User id
@@ -30,7 +32,7 @@ public class User {
         this.email = email;
     }
 
-    @JsonProperty("id")
+//    @JsonProperty("id")
     public UUID getUserUid() {
         return userUid;
     }
--- learning-spring-boot/src/test/java/com/wtrungvu/learningspringboot/LearningSpringBootApplicationTests.java
+++ learning-spring-boot/src/test/java/com/wtrungvu/learningspringboot/LearningSpringBootApplicationTests.java
@@ -1,13 +1,45 @@
 package com.wtrungvu.learningspringboot;
 
+import com.wtrungvu.learningspringboot.clientproxy.UserResourceV1;
+import com.wtrungvu.learningspringboot.model.User;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
-class LearningSpringBootApplicationTests {
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class LearningSpringBootApplicationTests {
+
+	@Autowired
+	private UserResourceV1 userResourceV1;
 
 	@Test
-	void contextLoads() {
+	public void itShouldFetchAllUsers() {
+		List<User> users = userResourceV1.fetchUsers(null);
+
+		assertThat(users).hasSize(1);
+
+		User johnUser = new User(
+				null,
+				"John",
+				"Cena",
+				User.Gender.MALE,
+				25,
+				"john.cena123456@gmail.com"
+		);
+
+		assertThat(users.get(0)).isEqualToIgnoringGivenFields(johnUser, "userUid");
+
+		assertThat(users.get(0).getUserUid()).isInstanceOf(UUID.class);
+
+		assertThat(users.get(0).getUserUid()).isNotNull();
 	}
 
 }
