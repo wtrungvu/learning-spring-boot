da284c74669772437dfbc5db39e502e5a854aa73
Duc Trung Vu <wtrungvu@gmail.com>
2022-03-10 10:44:13 +0700

    Delete user Integration Test✨

--- learning-spring-boot/src/test/java/com/wtrungvu/learningspringboot/LearningSpringBootApplicationTests.java
+++ learning-spring-boot/src/test/java/com/wtrungvu/learningspringboot/LearningSpringBootApplicationTests.java
@@ -8,9 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.ws.rs.NotFoundException;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -38,5 +40,34 @@ public class LearningSpringBootApplicationTests {
 		//Then
 		User user = userResourceV1.fetchUser(johnUserUid);
 		assertThat(user).isEqualToComparingFieldByField(johnUser);
+	}
+
+	@Test
+	public void shouldDeleteUser() {
+		//Given
+		UUID johnUserUid = UUID.randomUUID();
+		User johnUser = new User(
+				johnUserUid,
+				"John",
+				"Cena",
+				User.Gender.MALE,
+				25,
+				"john.cena123456@gmail.com"
+		);
+
+		//When
+		userResourceV1.insertNewUser(johnUser);
+
+		//Then
+		User user = userResourceV1.fetchUser(johnUserUid);
+		assertThat(user).isEqualToComparingFieldByField(johnUser);
+
+		//When
+		userResourceV1.deleteUser(johnUserUid);
+
+		//Then
+		assertThatThrownBy(() -> userResourceV1.fetchUser(johnUserUid))
+				.isInstanceOf(NotFoundException.class);
+
 	}
 }
